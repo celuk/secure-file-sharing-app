58d8fb0e4e9d9e9bba7102ac2dd82c778abacd75
celuk <seyyid4091@gmail.com>
2024-02-12 23:59:19 +0300

    add whitelist rule

--- sec-fshare.py
+++ sec-fshare.py
@@ -14,12 +14,45 @@ parser.add_argument(
 parser.add_argument(
     "-fo", "--folder", type=str, help="Specify full folder path", default="."
 )
+parser.add_argument(
+    "-ips",
+    "--iplist",
+    type=str,
+    help="Specify ip whitelist for authorized access",
+    default="",
+)
 args = parser.parse_args()
 
 ## argumanlara kisitla ekleyebilirim, sadece o iplerde indirilebilsin diye, bu da ek özellik olur
 ## dosyanın folder olup olmadığını sen anla
 
 UPLOAD_FOLDER = args.filepath
+
+
+# https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib/28950776#28950776
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        s.connect(("10.255.255.255", 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = "127.0.0.1"
+    finally:
+        s.close()
+    return IP
+
+
+if "".join(args.iplist.split()) != "":
+    iplist = [each_ip.strip() for each_ip in args.iplist.split(",")]
+    iplist[:] = [x for x in iplist if x.strip()]
+
+allowed_ips = [get_ip()]
+
+# verilen ip listesini izin verilen (beyaz) listeye ekle
+for each_ip in iplist:
+    if each_ip not in allowed_ips:
+        allowed_ips.append(each_ip)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -29,6 +62,12 @@ def upload_file():
         file.save(os.path.join(UPLOAD_FOLDER, file.filename))
         return "File uploaded successfully"
     else:
+        client_ip = request.remote_addr  # istemci ip adresi
+        if client_ip not in allowed_ips:
+            return (
+                "Access Denied. Unauthorized IP Address!",
+                403,
+            )  # beyaz listede degilse yetkisiz erisim
         files = os.listdir(UPLOAD_FOLDER)
         file_list = "<br>".join(
             [f'<a href="/downloads/{file}">{file}</a>' for file in files]
@@ -38,7 +77,7 @@ def upload_file():
         <title>Upload and Download Files</title>
         <h1>Upload a File</h1>
         <div id="drop_area" style="padding:100px; border: 1px solid black">
-            Dosyalarınızı buraya sürükleyip bırakarak yükleyin.
+            Drag and drop files here to upload
         </div>
         <input type="file" id="file_input">
         <button id="upload_button">Upload</button>
@@ -137,23 +176,15 @@ def upload_file():
 
 @app.route("/downloads/<path:filename>", methods=["GET", "POST"])
 def download(filename):
+    client_ip = request.remote_addr  # istemci ip adresi
+    if client_ip not in allowed_ips:
+        return (
+            "Access Denied. Unauthorized IP Address!",
+            403,
+        )  # beyaz listede degilse yetkisiz erisim
     return send_from_directory(
         directory=UPLOAD_FOLDER, path=filename, as_attachment=True
     )
-
-
-def get_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.settimeout(0)
-    try:
-        # doesn't even have to be reachable
-        s.connect(("10.255.255.255", 1))
-        IP = s.getsockname()[0]
-    except Exception:
-        IP = "127.0.0.1"
-    finally:
-        s.close()
-    return IP
 
 
 if __name__ == "__main__":
